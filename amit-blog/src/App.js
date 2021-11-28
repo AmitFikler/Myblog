1f7680cd73a3d55e6e13c585db8a3390e7025857
AmitFikler <amitfikler@gmail.com>
2021-11-28 16:43:02 +0200

    build react skeleton

---
+++ amit-blog/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1 className="header">App Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
